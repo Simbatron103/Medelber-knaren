5eba3248560b3e45f4ab65c18b4a3d698c0cb5bd
Jonathan Lövgren <jonathan.lovgren@skola.sala.se>
2024-08-28 13:38:43 +0200

    Add project files.

---
+++ Medelberäknaren/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Medelberäknaren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett heltal");
+            double sum = 0.0;
+            double numbers = 0.0;
+            while (true)
+            {
+
+
+                Double number = Convert.ToDouble(Console.ReadLine());
+                
+                numbers++;
+                sum = sum + number;
+                double medelvärdet = sum / numbers;
+                Console.WriteLine("medelvärdet är " + medelvärdet);
+            
+              
+            }
+        }
+    }
+}
